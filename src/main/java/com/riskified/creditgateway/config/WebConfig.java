11f3e817bed6b0e7c4839e9ca393e65b4f67a7db
erezSky <erezsky@gmail.com>
2021-02-11 21:46:43 +0200

    rest template and retry template config

--- src/main/java/com/riskified/creditgateway/config/WebConfig.java
+++ src/main/java/com/riskified/creditgateway/config/WebConfig.java
@@ -1,14 +1,36 @@
 package com.riskified.creditgateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableRetry
 public class WebConfig {
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.create();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public RetryTemplate retryTemplate(@Value("${application.retry.count:3}") Integer retryCount,
+                                       @Value("${application.retry.initial:1}") Integer initial) {
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy(retryCount);
+
+        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+        backOffPolicy.setInitialInterval(initial);
+        backOffPolicy.setMultiplier(2);
+
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+
+        return retryTemplate;
     }
 }
