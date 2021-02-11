b4abbde14559b3cbbfe5978c9f7a7c079a69180d
erezSky <erezsky@gmail.com>
2021-02-11 16:27:41 +0200

    factory tests

---
+++ src/test/java/com/riskified/creditgateway/CreditCompanyFactoryTest.java
@@ -0,0 +1,27 @@
+package com.riskified.creditgateway;
+
+import com.riskified.creditgateway.enums.CreditCompanyType;
+import com.riskified.creditgateway.factories.CreditCompanyFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CreditCompanyFactoryTest {
+
+    @Autowired
+    private CreditCompanyFactory factory;
+
+    @Test
+    void verify_all_company_types_exist() {
+        Stream.of(CreditCompanyType.values())
+                .forEach(creditCompany -> {
+                    var company = factory.getCreditCompany(creditCompany);
+                    assertNotNull(company);
+                });
+    }
+}
