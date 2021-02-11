0ae4102cb079a6d12840342a9984ea301448c977
erezSky <erezsky@gmail.com>
2021-02-11 23:31:48 +0200

    VisaSuccessStatus values

---
+++ src/main/java/com/riskified/creditgateway/enums/VisaSuccessStatus.java
@@ -0,0 +1,6 @@
+package com.riskified.creditgateway.enums;
+
+public class VisaSuccessStatus {
+    public static final String SUCCESS = "Success";
+    public static final String FAILURE = "Failure";
+}
