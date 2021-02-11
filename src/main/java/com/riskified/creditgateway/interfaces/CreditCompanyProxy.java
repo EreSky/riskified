39d74f64f8ad7a0163d874f66cd4692d6a81a30b
erezSky <erezsky@gmail.com>
2021-02-11 21:49:53 +0200

    throw business exception

--- src/main/java/com/riskified/creditgateway/interfaces/CreditCompanyProxy.java
+++ src/main/java/com/riskified/creditgateway/interfaces/CreditCompanyProxy.java
@@ -1,7 +1,8 @@
 package com.riskified.creditgateway.interfaces;
 
 import com.riskified.creditgateway.dtos.ChargeRequest;
+import com.riskified.creditgateway.exceptions.BusinessException;
 
 public interface CreditCompanyProxy {
-    void chargeRequest(String merchantId, ChargeRequest chargeRequest);
+    void chargeRequest(String merchantId, ChargeRequest chargeRequest) throws BusinessException;
 }
