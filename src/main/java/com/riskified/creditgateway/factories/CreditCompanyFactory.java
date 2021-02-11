1809b98795303fed74f3cd6e8bd3c79cd44c7f38
erezSky <erezsky@gmail.com>
2021-02-11 16:26:32 +0200

    creditCardCompany factory

---
+++ src/main/java/com/riskified/creditgateway/factories/CreditCompanyFactory.java
@@ -0,0 +1,24 @@
+package com.riskified.creditgateway.factories;
+
+import com.riskified.creditgateway.interfaces.BaseCreditCompany;
+import com.riskified.creditgateway.enums.CreditCompanyType;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CreditCompanyFactory {
+    private final Map<CreditCompanyType, BaseCreditCompany> companyMapping = new HashMap<>();
+
+    public CreditCompanyFactory(List<BaseCreditCompany> creditCompanyList) {
+        creditCompanyList.forEach(baseCreditCompany -> {
+            companyMapping.put(baseCreditCompany.getCompany(), baseCreditCompany);
+        });
+    }
+
+    public BaseCreditCompany getCreditCompany(CreditCompanyType creditCompanyType){
+        return companyMapping.getOrDefault(creditCompanyType, null);
+    }
+}
