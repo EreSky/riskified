0c4e5dd1cb1046ad54114c3e682828d2431aa9de
erezSky <erezsky@gmail.com>
2021-02-11 21:50:34 +0200

    visa service logic

--- src/main/java/com/riskified/creditgateway/creditcompanies/visa/VisaCreditCompany.java
+++ src/main/java/com/riskified/creditgateway/creditcompanies/visa/VisaCreditCompany.java
@@ -1,26 +1,32 @@
 package com.riskified.creditgateway.creditcompanies.visa;
 
-import com.riskified.creditgateway.interfaces.BaseCreditCompany;
-import com.riskified.creditgateway.enums.CreditCompanyType;
 import com.riskified.creditgateway.creditcompanies.visa.dtos.VisaChargeRequest;
 import com.riskified.creditgateway.creditcompanies.visa.dtos.VisaChargeResult;
 import com.riskified.creditgateway.dtos.ChargeRequest;
+import com.riskified.creditgateway.enums.CreditCompanyType;
+import com.riskified.creditgateway.exceptions.BusinessException;
+import com.riskified.creditgateway.interfaces.BaseCreditCompany;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.retry.RetryException;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Slf4j
 @Component
 public class VisaCreditCompany extends BaseCreditCompany {
-    @Value("${application.visa.base-url}")
-    private String creditCardCompanyUrl;
 
-    public VisaCreditCompany(WebClient webClient) {
-        super(webClient);
+    public VisaCreditCompany(RestTemplate restTemplate,
+                             @Value("${application.visa.base-url}") String baseUrl) {
+        super(restTemplate);
+//        restTemplate.setUriTemplateHandler(new RootUriTemplateHandler(baseUrl));
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(baseUrl));
     }
 
     @Override
@@ -28,38 +34,35 @@ public class VisaCreditCompany extends BaseCreditCompany {
         return CreditCompanyType.Visa;
     }
 
-
     @Override
-    public void chargeRequest(String merchantId, ChargeRequest chargeRequest) {
-        var request = webClient.post()
-                .uri(creditCardCompanyUrl + "/chargeCard", creditCardCompanyUrl)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .header("identifier", chargeRequest.getFullName().trim().split(" ")[0])
-                .body(Mono.just(
-                        VisaChargeRequest.builder()
-                                .fullName(chargeRequest.getFullName())
-                                .number(chargeRequest.getCreditCardNumber())
-                                .expiration(chargeRequest.getExpirationDate())
-                                .cvv(chargeRequest.getCvv())
-                                .totalAmount(chargeRequest.getAmount())
-                                .build()),
-                        ChargeRequest.class);
+    @Retryable(value = RetryException.class)
+    public void chargeRequest(String merchantId, ChargeRequest chargeRequest) throws BusinessException {
+        var headers = new HttpHeaders();
+        headers.add("identifier", chargeRequest.getFullName().trim().split(" ")[0]);
 
-        request.exchangeToMono(response -> {
-            log.info(response.statusCode().toString());
+        HttpEntity<VisaChargeRequest> request =
+                new HttpEntity<>(VisaChargeRequest.builder()
+                        .fullName(chargeRequest.getFullName())
+                        .number(chargeRequest.getCreditCardNumber())
+                        .expiration(chargeRequest.getExpirationDate())
+                        .cvv(chargeRequest.getCvv())
+                        .totalAmount(chargeRequest.getAmount())
+                        .build(), headers);
+        try {
+            var response = restTemplate
+                    .exchange("/chargeCard", HttpMethod.POST, request, VisaChargeResult.class);
 
-            if (response.statusCode()
-                    .equals(HttpStatus.OK)) {
-                return response.bodyToMono(VisaChargeResult.class);
-            } else if (response.statusCode()
-                    .is4xxClientError()) {
-                return Mono.just("Error response");
-            } else {
-                return response.createException()
-                        .flatMap(Mono::error);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                var body = response.getBody();
+                if (body == null) {
+                    throw new BusinessException();
+                } else if (!"Success".equals(body.getChargeResult())) {
+                    throw new BusinessException(body.getResultReason());
+                }
             }
-        });
+        } catch (HttpStatusCodeException e) {
+            log.error("failed to post charge request: {}, time: {}", e.getMessage(), System.currentTimeMillis());
+            throw new RetryException(e.getMessage(), e);
+        }
     }
-
 }
