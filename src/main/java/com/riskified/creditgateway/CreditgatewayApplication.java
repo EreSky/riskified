ba8a89417dee216497ae676c992a0baa30625c93
erezSky <erezsky@gmail.com>
2021-02-11 12:20:32 +0200

    initial commit

---
+++ src/main/java/com/riskified/creditgateway/CreditgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.riskified.creditgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditgatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/riskified/creditgateway/CreditgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riskified.creditgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
